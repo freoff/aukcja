ecf0574164198321549d5fb31661fd0ffd148184
freo <kpodlaski@gmail.com>
2017-05-10 01:44:03 +0200

    delete some compiled file in src folder

--- src/controller/api.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express = require("express");
-var aukcja_1 = require("./api/aukcja");
-exports.router = express.Router();
-exports.router.use('/aukcja', aukcja_1.router); //root api
-///blb 
--- src/controller/compileDir.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-/**
- * Created by freo on 28.04.17.
- */
-console.log('Only to create directory structure in app');
